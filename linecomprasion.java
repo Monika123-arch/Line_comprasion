a21d636582769081e54d456f0707887a2fc20e80
Monika <monikadkale98@gmail.com>
2021-02-12 15:05:34 +0530

    added new welcome message

--- linecomprasion.java
+++ linecomprasion.java
@@ -1,14 +1,37 @@
-import java.util.Scanner;
 public class linecomprasion {
-        public static void main(String[] args) {
-		Scanner myobj = new Scanner(System.in);
-		System.out.println("enter x1 and y1 values");
-		int x1 = myobj.nextInt();
-		int y1 = myobj.nextInt();
-		System.out.println("enter x2 and y2 values");
-                int x2 = myobj.nextInt();
-                int y2 = myobj.nextInt();
-		double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-		System.out.println( length);
-        }
+
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+
+    public linecomprasion(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    static void compareTo(double length1, double length2) {
+        if (length1 == length2)
+            System.out.println("length1 is equal to length2");
+        else if (length1 > length2)
+            System.out.println("length1 is greater than length2");
+        else
+            System.out.println("length1 is shorter than length2");
+    }
+
+    static double calculate_length(linecomprasion obj) {
+        double length = Math.sqrt(Math.pow(obj.x2 - obj.x1, 2) + Math.pow(obj.y2 - obj.y1, 2));
+        return length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("***welcome line comprasion problem***");
+        linecomprasion line1 = new linecomprasion(2.3,3.5,5.3,4.5);
+        linecomprasion line2 = new linecomprasion(3.5,4.6,8.5,4.5);
+        double length1 = calculate_length(line1);
+        double length2 = calculate_length(line2);
+        compareTo(length1, length2);
+    }
 }
